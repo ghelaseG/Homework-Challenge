255314f668cb48bf3956940e5c01db07d1f1e71e
ghelaseG <ghelase.georgian@gmail.com>
2023-11-21 12:21:29 +0000

    some changes

--- Arrays/minmax.py
+++ Arrays/minmax.py
@@ -1,3 +1,18 @@
 # Pseudocode 
 
-# functions
+
+# Find the minimum and maximum element in an array without built-in array methods:
+
+# Start by checking if the array is empty. If it is, return a message indicating that the array is empty.
+
+# Initialize variables min_element and max_element with the first element of the array.
+
+# Iterate through the array.
+
+# For each element, compare it with the current min_element and max_element.
+
+# Update min_element if the current element is smaller.
+
+# Update max_element if the current element is larger.
+
+# After iterating through the array, return a tuple containing the minimum and maximum elements.
