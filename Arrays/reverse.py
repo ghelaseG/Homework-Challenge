0d2fe60a7f408882b80c500b05fdd3172360901f
ghelaseG <ghelase.georgian@gmail.com>
2023-11-21 22:26:58 +0000

    pseudocode

--- Arrays/reverse.py
+++ Arrays/reverse.py
@@ -1,1 +1,10 @@
 # Write a program to reverse the array without built-in array methods
+
+# Pseudocode:
+
+## Initialize variables start and end to represent the start and end indices of the array.
+## Use a while loop to iterate until start is less than end.
+## Inside the loop, swap the elements at indices start and end.
+## Move start index towards the end of the array and end index towards the start of the array.
+## Continue the loop until start is no longer less than end.
+## Return the reversed array.
