59d54a7096bde7c01f93ae15307ea2267c363855
ghelaseG <ghelase.georgian@gmail.com>
2023-11-22 23:03:25 +0000

    kinda completed

--- Arrays/reverse.py
+++ Arrays/reverse.py
@@ -1,10 +1,25 @@
 # Write a program to reverse the array without built-in array methods
 
-# Pseudocode:
+## Find the length of the array 
+def reverseArr(array):
+    array_length = 0
+    for _ in array:
+        array_length += 1
 
 ## Initialize variables start and end to represent the start and end indices of the array.
+    start = 0
+    end = array_length - 1
+
 ## Use a while loop to iterate until start is less than end.
-## Inside the loop, swap the elements at indices start and end.
-## Move start index towards the end of the array and end index towards the start of the array.
-## Continue the loop until start is no longer less than end.
+    while start < end:
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
+        
 ## Return the reversed array.
+    return array
+
+# Example usage:
+exp_array = [10, 20, 30, 40, 50]
+reverseArr(exp_array)
+print("Reversed Array:", exp_array)
