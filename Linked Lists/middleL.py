f7e8a0918dedcfa18829ead8ecbee9ccdcf9f47d
ghelaseG <ghelase.georgian@gmail.com>
2023-11-26 22:49:11 +0000

    pseudocode

---
+++ Linked Lists/middleL.py
@@ -0,0 +1,15 @@
+# Pseudocode
+
+
+# Employ two pointers: slow and fast.
+
+# Traverse the list with both pointers:
+#1) Start both at the head.
+#2) Move fast twice as quickly as slow.
+#3) When fast reaches the end, slow will be at the middle.
+
+# Delete the middle node:
+# Reassign the next pointer of the node before slow to skip slow.
+
+# Handle edge cases:
+# Account for an empty list or a list with fewer than three nodes.
---
+++ Linked Lists/palindromeL.py
@@ -0,0 +1,15 @@
+# Pseudocde
+
+# Traverse the linked list to find its length.
+
+# Initialize two pointers: slow and fast.
+
+# Move fast twice as quickly as slow to find the middle.
+
+# Reverse the second half of the linked list starting from slow.
+
+# Compare the first half with the reversed second half.
+
+# If they match, the linked list is a palindrome.
+
+# Restore the reversed second half to maintain the original structure.
