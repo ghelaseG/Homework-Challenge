89cf43f6383617b67266fda447cba4c0bacb01a6
ghelaseG <ghelase.georgian@gmail.com>
2023-11-27 22:06:08 +0000

    completed

--- Linked Lists/reverseL.py
+++ Linked Lists/reverseL.py
@@ -1,50 +1,31 @@
-## Pseudocde
+# Definition of a node in a linked list
+class ListNode:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
 
-# Write a function to reverse the nodes of a linked list
+# Function to reverse a linked list
+def reverse_linked_list(head):
+# Initialize two pointers: prev (previous) and current
+    prev, current = None, head
 
-# Use three pointers: current, prev, and next_node.
+# Traverse the linked list
+    while current:
+# Swap the next pointer of the current node to point to the previous node
+        current.next, prev, current = prev, current, current.next
 
-    def reverse(head):
-        if head == None:
-            return head
-        current = head
-        prev = None
-        next_node = None
-        while current!= None:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        head = prev
-        return head
+# The head of the reversed list is the last node
+    return prev
 
-# Traverse through each node:
-# 1) Store the next pointer in temp variable.
-# 2) Change the link of the current node to point to previous node (prev).
-# 3) Move both the prev and curr one step forward.
+# Example Usage:
+# Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 
+# Reverse the linked list
+reversed_head = reverse_linked_list(head)
 
-    ## Pseudocode
-    class Node:
-        def __init__(self, value):
-            self.value = value
-            self.next = None
-            self.prev = None
-            def reverseLinkedList(head):
-                if head == None:
-                    return head
-                current = head
-                prev = None
-                next_node = None
-                while current!= None:
-                    next_node = current.next
-                    current.next = prev
-                    prev = current
-                    current = next_node
-                head = prev
-                return head
-            self.reverseLinkedList = reverseLinkedList
-
-# Update the head to the last node.
-# Account for scenarios with no list or only one node.
-# Furnish the head of the reversed linked list.
+# Print the reversed linked list
+current = reversed_head
+while current:
+    print(current.value, end=" -> ")
+    current = current.next
