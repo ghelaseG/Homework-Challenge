244127dc11f7570bfcdea0af3502e00ba788a557
ghelaseG <ghelase.georgian@gmail.com>
2023-11-28 21:59:41 +0000

    complete

--- Linked Lists/middleL.py
+++ Linked Lists/middleL.py
@@ -1,15 +1,38 @@
-# Pseudocode
-# Delete middle of linked list.
+class Friend:
+    def __init__(self, number=0, next_friend=None):
+        self.number = number
+        self.next = next_friend
 
-# Employ two pointers: slow and fast.
+def remove_middle_friend(start_friend):
+    # Check if there are enough friends to remove from the middle
+    if not start_friend or not start_friend.next or not start_friend.next.next:
+        return None
 
-# Traverse the list with both pointers:
-#1) Start both at the head.
-#2) Move fast twice as quickly as slow.
-#3) When fast reaches the end, slow will be at the middle.
+    slow_friend = fast_friend = start_friend
+    previous_friend = None
 
-# Delete the middle node:
-# Reassign the next pointer of the node before slow to skip slow.
+    # Move the fast pointer twice as quickly as the slow pointer to find the middle
+    while fast_friend and fast_friend.next and fast_friend.next.next:
+        previous_friend = slow_friend
+        slow_friend = slow_friend.next
+        fast_friend = fast_friend.next.next
 
-# Handle edge cases:
-# Account for an empty list or a list with fewer than three nodes.
+    # Remove the middle friend by updating the next pointer of the previous friend
+    if previous_friend:
+        previous_friend.next = slow_friend.next
+    else:
+        start_friend = slow_friend.next
+
+    return start_friend
+
+# Example:
+start_friend = Friend(1, Friend(2, Friend(3, Friend(4, Friend(5)))))
+
+# Remove the middle friend
+new_start_friend = remove_middle_friend(start_friend)
+
+# Print the modified list
+current_friend = new_start_friend
+while current_friend:
+    print(current_friend.number, end=" -> ")
+    current_friend = current_friend.next
